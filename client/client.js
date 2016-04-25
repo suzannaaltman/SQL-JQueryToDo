8cd4767feb5346cc9d993cd6329788ebb372e239
suzannaaltman <suzannaaltman@gmail.com>
2016-04-24 21:38:08 -0500

    working on checkbox

--- client/client.js
+++ client/client.js
@@ -4,23 +4,26 @@ $(function(){
     console.log(response);
   });
 
-  $("addTask").on("submit", function(){
+  $("#addTask").on("submit", function(event){
+    event.preventDefault();
     console.log("addTask clicked");
-    $.post("/toDo/add", {"item": response}).done(function(response){
+    $.post("/toDo/", {"item": response}).done(function(response){
       console.log('response.item:', response);
     });
     addTask();
   })
 
 
+
   function addTask(response){
     $.each(response, function(index) {
-        $("#itemHolder").append("<div class=\"well\">" + response[index].item + "</div><br />");
+        $("#itemHolder").append("<div class=\"well\"><input type=\"checkbox\" class=\"done\" value=\"checked\" />" + response[index].item + "</div><br />");
         console.log(response[index].completed);
         if((response[index].completed).toString() === "false"){
           $( "div.well" ).addClass( "notDone");
         }
     });
   };
+
 
 })
