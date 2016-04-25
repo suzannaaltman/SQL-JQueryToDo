ff2e896b0c7f3b6fabc1b162e25c6d57faacf549
suzannaaltman <suzannaaltman@gmail.com>
2016-04-25 11:22:44 -0500

    css working

--- client/client.js
+++ client/client.js
@@ -15,11 +15,14 @@ $(function(){
 
   function addTask(){
     $.get("/toDo").done(function(response){
+      $("#itemHolder").children().remove();
       $.each(response, function(index) {
           $("#itemHolder").append("<div class=\"well\"><input type=\"checkbox\" class=\"check\" />" + response[index].item + "</div><br />");
-          console.log(response[index].completed);
+          console.log('completed:', response[index].completed);
+          console.log('boolean:', Boolean(response[index].completed));
           if(Boolean(response[index].completed) === true){
-            $( "div.well" ).addClass( "notDone");
+            console.log('true conditional');
+            $( "div.well" ).addClass( "done");
           }
       });
       console.log('get called', response);
